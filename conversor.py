478633004b1e0e27a4409b24afb42ea816d29d6e
davimatcor3 <acostamorenodavidmateo@gmail.com>
2021-11-08 14:46:53 -0500

    se agrega valor para reducir el decimal

--- conversor.py
+++ conversor.py
@@ -2,5 +2,6 @@ pesos = input("cuantos pesos colombianos tienes?: ")
 pesos = float(pesos)    #es igual a int y transforma un valor o una variable a un decimal 
 valor_dolar = 3750
 dolares = pesos / valor_dolar
+dolares = round(dolares, 2) #los reducimos a dos decimales y la guardamos en la variable dolares 
 dolares = str(dolares)
 print("tienes $" + dolares + " dolares")
