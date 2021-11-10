b5ff3d68b8d8a6016a0b695397c65841aa11b542
davimatcor3 <acostamorenodavidmateo@gmail.com>
2021-11-09 19:39:48 -0500

    se crea carpeta de condicionales

--- conversor.py
+++ conversor.py
@@ -1,7 +1,7 @@
 pesos = input("cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)    #es igual a int y transforma un valor o una variable a un decimal 
+pesos = float(pesos)           #es igual a int y transforma un valor o una variable a un decimal 
 valor_dolar = 3750
 dolares = pesos / valor_dolar
-dolares = round(dolares, 2) #los reducimos a dos decimales y la guardamos en la variable dolares 
+dolares = round(dolares, 2)    #los reducimos a dos decimales y la guardamos en la variable dolares 
 dolares = str(dolares)
 print("tienes $" + dolares + " dolares")
